9375ac7a4300644dc73354bdba6bdd39ce633f2e
Nathan Sala <sala.nathan@gmail.com>
2025-03-18 18:08:17 +0900

    disable more fx-portal migrations on amoy

--- migrations/token/ERC20/polygon_bridging/fx-portal/FxERC20FixedSupplyRootTunnel@2.0_deploy.js
+++ migrations/token/ERC20/polygon_bridging/fx-portal/FxERC20FixedSupplyRootTunnel@2.0_deploy.js
@@ -1,6 +1,6 @@
 const Contract_deploy = require('../../../../../src/templates/Contract/deploy');
 const {getNamedAccount, getContractAddress} = require('../../../../../src/helpers/templates');
-const {skipChainTypesExceptFor} = require('../../../../../src/helpers/common');
+const {multiSkip, skipNetworks, skipChainTypesExceptFor} = require('../../../../../src/helpers/common');
 
 module.exports = Contract_deploy('FxERC20FixedSupplyRootTunnel@2.0', {
   contract: 'FxERC20FixedSupplyRootTunnel',
@@ -13,6 +13,9 @@ module.exports = Contract_deploy('FxERC20FixedSupplyRootTunnel@2.0', {
   ],
   deterministicDeployment: true,
 });
-module.exports.skip = skipChainTypesExceptFor('ethereum');
+module.exports.skip = multiSkip(
+  skipNetworks(['sepolia']), // until fx-portal is supported on amoy
+  skipChainTypesExceptFor('ethereum'),
+);
 module.exports.tags = ['ERC20', 'PolygonFxPortal'];
 module.exports.dependencies = ['ForwarderRegistry@4.1_deploy'];
