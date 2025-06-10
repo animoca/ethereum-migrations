a70dfea1440557797e2a5d6586f4b55e06a3eebe
samyung-ab <samyung@animocabrands.com>
2025-06-10 13:17:50 +0800

    Fixed wrong address of Chainlink VRF Wrapper V2 address in TokenMetadataResolverRandomizedReveal

--- migrations/token/metadata/TokenMetadataResolverRandomizedReveal@4.1_deploy.js
+++ migrations/token/metadata/TokenMetadataResolverRandomizedReveal@4.1_deploy.js
@@ -1,6 +1,7 @@
 const {ethers} = require('hardhat');
 const {getContractAddressOrFallback} = require('../../../src/helpers/templates');
 const Contract_deploy = require('../../../src/templates/Contract/deploy');
+const {skipNetworksTagged} = require('../../../src/helpers/common');
 
 module.exports = Contract_deploy('TokenMetadataResolverRandomizedReveal@4.1', {
   contract: 'TokenMetadataResolverRandomizedReveal',
@@ -12,3 +13,4 @@ module.exports = Contract_deploy('TokenMetadataResolverRandomizedReveal@4.1', {
   deterministicDeployment: true,
 });
 module.exports.tags = ['TokenMetadata'];
+module.exports.skip = skipNetworksTagged('base');
